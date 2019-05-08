d8f732fcb14fd8f1f220f0fdbfd251d1b642651e
jennarchibald <jennarchibald91@gmail.com>
2019-05-08 11:29:58 +0100

    dining room

--- src/main/java/DiningRoom.java
+++ src/main/java/DiningRoom.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DiningRoom {
+
+    private ArrayList<Integer> tables;
+    private ArrayList<Guest> guests;
+
+    public DiningRoom(ArrayList<Integer> tables){
+        this.tables = tables;
+        this.guests = new ArrayList<>();
+    }
+
+    public int capacity() {
+        int capacity = 0;
+        for (int i = 0; i < tables.size(); i++){
+            capacity += tables.get(i);
+        }
+        return capacity;
+    }
+
+
+    public int guestCount() {
+        return guests.size();
+    }
+
+    public void addGuest(Guest guest) {
+        guests.add(guest);
+    }
 }
---
+++ src/test/java/DiningRoomTest.java
@@ -0,0 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class DiningRoomTest {
+
+    DiningRoom diningRoom;
+    Guest guest;
+    ArrayList<Integer> tables;
+
+    @Before
+    public void before(){
+        tables = new ArrayList<Integer>();
+        tables.add(4);
+        tables.add(2);
+        tables.add(6);
+        diningRoom = new DiningRoom(tables);
+        guest = new Guest();
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(12, diningRoom.capacity());
+    }
+
+    @Test
+    public void hasGuests(){
+        assertEquals(0, diningRoom.guestCount());
+    }
+
+    @Test
+    public void canAddGuest(){
+        diningRoom.addGuest(guest);
+        assertEquals(1, diningRoom.guestCount());
+    }
+}
