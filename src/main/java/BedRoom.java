623e532827cb60db7ea278f645f658871697930b
jennarchibald <jennarchibald91@gmail.com>
2019-05-08 11:49:55 +0100

    bedroom

--- src/main/java/BedRoom.java
+++ src/main/java/BedRoom.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class BedRoom {
+
+    private int roomNumber;
+    private int capacity;
+    private ArrayList<Guest> guests;
+    private String type;
+
+    public BedRoom(int roomNumber, int capacity, String type) {
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+        this.guests = new ArrayList<>();
+        this.type = type;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int guestCount() {
+        return guests.size();
+    }
+
+    public void addGuest(Guest guest) {
+        guests.add(guest);
+    }
+
+    public void removeGuest(Guest guest) {
+        guests.remove(guest);
+    }
 }
--- src/test/java/BedRoomTest.java
+++ src/test/java/BedRoomTest.java
@@ -1,2 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedRoomTest {
+
+    BedRoom bedRoom;
+    Guest guest;
+
+    @Before
+    public void before(){
+        bedRoom = new BedRoom(1, 2, "Twin");
+        guest = new Guest();
+    }
+
+    @Test
+    public void hasRoomNumber(){
+        assertEquals(1, bedRoom.getRoomNumber());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(2, bedRoom.getCapacity());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("Twin", bedRoom.getType());
+    }
+
+    @Test
+    public void hasGuestCount(){
+        assertEquals(0, bedRoom.guestCount());
+    }
+
+    @Test
+    public void canAddGuest(){
+        bedRoom.addGuest(guest);
+        assertEquals(1, bedRoom.guestCount());
+    }
+
+    @Test
+    public void canRemoveGuest(){
+        bedRoom.addGuest(guest);
+        bedRoom.removeGuest(guest);
+        assertEquals(0, bedRoom.guestCount());
+    }
 }
